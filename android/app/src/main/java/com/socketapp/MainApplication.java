f59838130592e48de50a3b3286191ac54cbad028
my0sot1s <manhte231@gmail.com>
2018-05-19 18:31:19 +0700

    fix splash

--- android/app/src/main/java/com/socketapp/MainApplication.java
+++ android/app/src/main/java/com/socketapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.brentvatne.react.ReactVideoPackage;
+import com.brentvatne.react.ReactVideoPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
 import org.reactnative.camera.RNCameraPackage;
@@ -39,6 +40,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactVideoPackage(),
             new ReactVideoPackage(),
             new ImagePickerPackage(),
             new RNSpinkitPackage(),
